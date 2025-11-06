d7cb643fecf5aa36a1f2c570d97a965bac9d715e
Roopesh O R <62459820+roopeshor@users.noreply.github.com>
2025-11-06 08:20:43 +0530

    Revert "rm other flavours"
    This reverts commit 384d6f3d333d65813226fe2507e3abaae9d78bff.

--- lib/app_flavor.dart
+++ lib/app_flavor.dart
@@ -2,22 +2,22 @@ enum AppFlavor { play, izzy, libre }
 
 extension ExtraAppFlavor on AppFlavor {
   bool get canEnableErrorReporting {
-    // switch (this) {
-    //   case AppFlavor.play:
-    //     return true;
-    //   case AppFlavor.izzy:
-    //   case AppFlavor.libre:
-    // }
-    return false;
+    switch (this) {
+      case AppFlavor.play:
+        return true;
+      case AppFlavor.izzy:
+      case AppFlavor.libre:
+        return false;
+    }
   }
 
   bool get hasMapStyleDefault {
-    // switch (this) {
-    //   case AppFlavor.play:
-    //     return true;
-    //   case AppFlavor.izzy:
-    //   case AppFlavor.libre:
-    // }
-    return false;
+    switch (this) {
+      case AppFlavor.play:
+        return true;
+      case AppFlavor.izzy:
+      case AppFlavor.libre:
+        return false;
+    }
   }
 }
